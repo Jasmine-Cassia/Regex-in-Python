768f4312cf7cb2c40c00bbb3ae82dbfba845af6c
Jasmine Cassia <58848359+Jasmine-Cassia@users.noreply.github.com>
2020-04-01 10:59:34 +0530

    Update findall.py

--- findall.py
+++ findall.py
@@ -2,3 +2,13 @@ import re
 txt = "The rain in India"
 x = re.findall("ai",txt)
 print(x)
+
+'''
+import re
+txt = "The rain in India"
+x = re.findall("Spain",txt)
+if x:
+    print("Yes, there is atleast one match")
+else:
+    print("No match found!")
+'''
